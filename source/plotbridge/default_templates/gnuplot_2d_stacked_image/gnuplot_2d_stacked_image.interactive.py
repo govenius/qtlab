ff6635b9abe3826d55f83f07ab02b8f88240036e
Joonas Govenius <joonas.govenius@aalto.fi>
2014-10-20 20:07:13 +0300

    Handle single traces and check that all "slow coordinates" are unique.

--- source/plotbridge/default_templates/gnuplot_2d_stacked_image/gnuplot_2d_stacked_image.interactive.py
+++ source/plotbridge/default_templates/gnuplot_2d_stacked_image/gnuplot_2d_stacked_image.interactive.py
@@ -50,7 +50,7 @@ try:
   refresh_lock()
   
   file_to_monitor = 'image_matrix.gnuplot_binary_matrix'
-  files_that_must_exist_before_initial_plot = [ file_to_monitor, plot_script ]
+  files_that_must_exist = [ file_to_monitor, plot_script ]
 
   plotted_once = False
 
@@ -61,38 +61,42 @@ try:
   gp = None
   while not plotted_once or gp.poll() == None: # keep polling as long as gnuplot is alive
 
-    if not plotted_once:
+    if all( os.path.exists(ff) for ff in files_that_must_exist):
+      if not plotted_once:
 
-      if all( os.path.exists(ff) for ff in files_that_must_exist_before_initial_plot):
-        # Initial plot
-        plot_last_changed_time = os.path.getmtime(file_to_monitor)
-        eps_last_changed_time = 0
-        gp = subprocess.Popen(['gnuplot', plot_script, '-'],
-                              stdin=subprocess.PIPE, stdout=sys.stdout, stderr=sys.stderr)
-        plotted_once = True
+          # Initial plot
+          plot_last_changed_time = os.path.getmtime(file_to_monitor)
+          eps_last_changed_time = 0
+          gp = subprocess.Popen(['gnuplot', plot_script, '-'],
+                                stdin=subprocess.PIPE, stdout=sys.stdout, stderr=sys.stderr)
+          plotted_once = True
 
-        print "Replotting every %g seconds (if plot script modification time changes)..." % replot_poll_period
-        print "Hit <ctrl> + C to exit."
+          print "Replotting every %g seconds (if plot script modification time changes)..." % replot_poll_period
+          print "Hit <ctrl> + C to exit."
 
-    else:
+      else:
 
-      # Update plot if the plot script was modified
-      plot_changed_time = os.path.getmtime(file_to_monitor)
-      if plot_changed_time != plot_last_changed_time and gp != None:
-        #logging.debug('Plot changed. Reloading plot script.')
-        gp.stdin.write('load "%s"\n' % plot_script)
-        plot_last_changed_time = plot_changed_time
-
-      # convert EPS to PDF
-      if os.path.isfile('output.eps'):
+        # Update plot if the plot script was modified
         try:
-          eps_changed_time = os.path.getmtime('output.eps')
-          if eps_changed_time != eps_last_changed_time:
-            eps_last_changed_time = eps_changed_time
-            with open(os.devnull, 'wb') as DEVNULL:
-              subprocess.call(['ps2pdf', '-dEPSCrop', 'output.eps'], stdin=None, stdout=DEVNULL, stderr=DEVNULL)
-        except:
-          pass # can fail because ps2pdf is not installed or because EPS is not fully generated yet
+          plot_changed_time = os.path.getmtime(file_to_monitor)
+          if plot_changed_time != plot_last_changed_time and gp != None:
+            #logging.debug('Plot changed. Reloading plot script.')
+            gp.stdin.write('load "%s"\n' % plot_script)
+            plot_last_changed_time = plot_changed_time
+
+          # convert EPS to PDF
+          if os.path.isfile('output.eps'):
+            try:
+              eps_changed_time = os.path.getmtime('output.eps')
+              if eps_changed_time != eps_last_changed_time:
+                eps_last_changed_time = eps_changed_time
+                with open(os.devnull, 'wb') as DEVNULL:
+                  subprocess.call(['ps2pdf', '-dEPSCrop', 'output.eps'], stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+            except:
+              pass # can fail because ps2pdf is not installed or because EPS is not fully generated yet
+
+        except OSError:
+          pass # the plot script does not exist which is normal if the plot was overwritten.
 
     time.sleep(replot_poll_period)
     refresh_lock()
